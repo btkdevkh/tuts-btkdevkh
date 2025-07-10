d1936b5e11d00c86d3c4fea5179db921898ce7b8
Bunthoeun Kong <amcjkbtk91@outlook.com>
2025-07-10 16:17:42 +0200

    feat: :sparkles: Add TS course

---
+++ cours/typescript-course/index.js
@@ -0,0 +1,4 @@
+var age = 35;
+var usename = "BK";
+console.log(age);
+console.log(usename);
---
+++ cours/typescript-course/index.ts
@@ -0,0 +1,5 @@
+const age: number = 35;
+const usename: string = "BK";
+
+console.log(age);
+console.log(usename);
