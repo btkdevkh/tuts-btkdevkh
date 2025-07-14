ea3a13c0d32263772ddc2102b87f534c1ceffce8
Bunthoeun Kong <amcjkbtk91@outlook.com>
2025-07-15 00:35:24 +0200

    feat: :sparkles: Add TS features course

--- cours/typescript-course/index.js
+++ cours/typescript-course/index.js
@@ -1,4 +1,74 @@
-var age = 35;
-var usename = "BK";
-console.log(age);
-console.log(usename);
+/**
+ * BASIC
+ *
+ * string
+ * number
+ * boolean
+ * null
+ * undefined
+ * union (|)
+ * [], Tuple
+ * {}
+ * any
+ * type (custom type)
+ * interface (object & class POO)
+ * unknown
+ *
+ * funtions
+ * void
+ * never
+ * ? (optional)
+ */
+var firstname = "Bella";
+var age = 10;
+var sexe = true;
+// union
+var user = null;
+user = "abc";
+// Tableau (Array)
+var array1 = ["abec", 100, true];
+var array2 = Array(1, 2, 3);
+// Tableau (Tuple)
+var array3 = ["Jim", 35, true];
+// Object
+var person = {
+    username: "JK007",
+    age: 35,
+    sexe: true,
+};
+// any
+var varAny = {};
+varAny = "sdsd";
+varAny = true;
+// unknown
+var unknownVar;
+unknownVar = "abc";
+unknownVar = 1000;
+console.log(unknownVar);
+var testVar;
+if (typeof unknownVar === "boolean") {
+    testVar = unknownVar;
+}
+// Funtions
+// function greet(nom: string): string {
+//   return `Hello, my name is ${nom}`;
+// }
+// const greet = function (nom: string): string {
+//   return `Hello, my name is ${nom}`;
+// };
+// const greet = (
+//   age: number,
+//   firstname: string = "Jimmy",
+//   msg?: string
+// ): string => {
+//   if (msg) {
+//     return `${msg}, my name is ${firstname}, I'm ${age} years old`;
+//   }
+//   return `Hello, my name is ${firstname}, I'm ${age} years old`;
+// };
+// const greet2 = (nom: string): void => {
+//   console.log(`Hello, my name is ${nom}`);
+// };
+// const throwException = (msg: string): never => {
+//   throw new Error(msg);
+// };
--- cours/typescript-course/index.ts
+++ cours/typescript-course/index.ts
@@ -1,5 +1,103 @@
-const age: number = 35;
-const usename: string = "BK";
+/**
+ * BASIC
+ *
+ * string
+ * number
+ * boolean
+ * null
+ * undefined
+ * union (|)
+ * [], Tuple
+ * {}
+ * any
+ * type (custom type)
+ * interface (object & class POO)
+ * unknown
+ *
+ * funtions
+ * void
+ * never
+ * ? (optional)
+ */
 
-console.log(age);
-console.log(usename);
+const firstname: string = "Bella";
+const age: number = 10;
+const sexe: boolean = true;
+
+// union
+let user: string | boolean | null | undefined = null;
+user = "abc";
+
+// Tableau (Array)
+const array1: (string | number | boolean)[] = ["abec", 100, true];
+const array2: number[] = Array(1, 2, 3);
+
+// Tableau (Tuple)
+const array3: [string, number, boolean] = ["Jim", 35, true];
+
+// Object
+const person: Person = {
+  username: "JK007",
+  age: 35,
+  sexe: true,
+};
+
+// any
+let varAny: any = {};
+varAny = "sdsd";
+varAny = true;
+
+// unknown
+let unknownVar: unknown;
+unknownVar = "abc";
+unknownVar = 1000;
+console.log(unknownVar);
+let testVar: boolean;
+if (typeof unknownVar === "boolean") {
+  testVar = unknownVar;
+}
+
+// Custom types
+type CustomVar = string | number | boolean;
+
+type Person = {
+  username: string;
+  age: number;
+  sexe: boolean;
+};
+
+// Interface (object & class POO)
+interface Person2 {
+  username: string;
+  age: number;
+  sexe: boolean;
+}
+
+// Funtions
+// function greet(nom: string): string {
+//   return `Hello, my name is ${nom}`;
+// }
+
+// const greet = function (nom: string): string {
+//   return `Hello, my name is ${nom}`;
+// };
+
+// const greet = (
+//   age: number,
+//   firstname: string = "Jimmy",
+//   msg?: string
+// ): string => {
+//   if (msg) {
+//     return `${msg}, my name is ${firstname}, I'm ${age} years old`;
+//   }
+
+//   return `Hello, my name is ${firstname}, I'm ${age} years old`;
+// };
+
+// const greet2 = (nom: string): void => {
+//   console.log(`Hello, my name is ${nom}`);
+// };
+
+// const throwException = (msg: string): never => {
+//   throw new Error(msg);
+// };
