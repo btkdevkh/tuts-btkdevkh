59f9022bf81e468ffaf3ca3f39c2253db8aa8f67
Bunthoeun Kong <amcjkbtk91@outlook.com>
2025-08-06 00:33:03 +0200

    feat: :sparkles: Add node feats

--- cours/nodejs-course/index.js
+++ cours/nodejs-course/index.js
@@ -1,3 +1,19 @@
-console.log("Hello World!");
-console.log(2 + 5);
-console.log(global);
+/**
+ * Node.js BASICS
+ *
+ * Read-Eval-Print-Loop (REPL)
+ * CommonJS Modules
+ * ES Modules
+ * NPM libraries
+ * Environement Variables
+ * Process Object
+ *
+ * FS Module
+ * URL Module
+ * Path Module
+ * OS Module
+ *
+ * HTTP Module
+ * API/HTTP Requests
+ * Middleware
+ */
---
+++ cours/nodejs-course/utils/utils.js
@@ -0,0 +1,7 @@
+const readArray = (array) => {
+  for (let arr of array) {
+    console.log(`élement : `, arr);
+  }
+};
+
+export { readArray };
