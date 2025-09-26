90fe691088351b808e3031530a0930ef4becc2b3
Bunthoeun Kong <amcjkbtk91@outlook.com>
2025-09-26 22:10:31 +0200

    feat/express-course: Work in progress...

---
+++ cours/express-course/README.md
@@ -0,0 +1,19 @@
+# Guides de ce cours Express.js
+
+## Sommaire :
+
+#1 Création d'un serveur avec Express
+#2 Les requêtes http & les réponses
+#3 Servir le dossier statique
+#4 Variables d'environement
+
+#5 L'aproache du WebApp classique et HTML Template
+#6 L'aproache d'API Rest et données en JSON
+#7 Middlewares
+#8 Gestion des erreurs
+
+#9 Organisation des dossiers
+#10 Controllers
+#11 Routes
+#12 Intéraction avec le frontend
+#13 Cors Origin
--- cours/express-course/src/server.ts
+++ cours/express-course/src/server.ts
@@ -1,1 +1,30 @@
-console.log(1234);
+/**
+ * #1 Création d'un serveur avec Express (x)
+ * #2 Les requêtes http & les réponses (x)
+ * #3 Servir le dossier statique (x)
+ * #4 Variables d'environement (x)
+ */
+
+import path from "path";
+import express, { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+
+// Load env var
+dotenv.config();
+
+// App
+const app = express();
+
+// Static folder
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+// Routes
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  // res.sendFile(path.join(__dirname, "..", "public", "home.html"));
+  res.status(200).json({ msg: "Bienvenue à mon API Rest!" });
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server listen on port ${PORT} : http://localhost:${PORT}`);
+});
